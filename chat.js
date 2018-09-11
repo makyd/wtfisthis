24d37f091809053436f8da80d61be49083f3e9d1
makyd <43168702+makyd@users.noreply.github.com>
2018-09-11 19:33:46 +0100

    Create chat.js

---
+++ chat.js
@@ -0,0 +1,36 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+client.on('ready', () => {
+    // list of servers
+    console.log("Connected as " + client.user.tag)
+    console.log("Servers:")
+    client.guilds.forEach((guild) =>{
+        console.log(" - " + guild.name)
+
+        // list of channels
+
+    guild.channels.forEach((channel) => {
+        console.log(` -- ${channel.name} (${channel.type}) - ${channel.id}`)
+})
+client.user.setActivity("The worlds smallest violine")
+
+})
+})
+
+client.on('message', (receivedMessage) => {
+    // Prevent bot from responding to its own messages
+    if (receivedMessage.author == client.user) {
+        return
+    }
+    
+    // Check if the bot's user was tagged in the message
+    if (receivedMessage == "Maky" || receivedMessage == "maky"||receivedMessage =="Makyd"||receivedMessage =="makyd") {
+        // Send acknowledgement message
+        receivedMessage.channel.send("<@184440359659503616>" + " Is a fucking Don")
+    }
+})
+
+bot_secret_token = "NDg5MDQ4NzgxODUwMDE3Nzky.Dnl7oA.wWsPhPa-15KtnUYuGN8W61GRat4"
+
+client.login(bot_secret_token)
